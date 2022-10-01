128ef49f815e61f706e65dabb7d4355c2ea180e3
Dylan King <dwkblue51@yahoo.com>
2022-10-01 09:56:46 -0500

    add CommentForm to Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { data } from "../data";
 import {useLoaderData} from 'react-router-dom';
+import { CommentForm } from "./CommentForm";
 
 export async function loader({params}) {
     const postData = await data.getSinglePost(params.id);
@@ -15,6 +16,7 @@ export const Post = () => {
         <h2>{title}</h2>
         <p>{content}</p>
         <div>
+            <CommentForm />
             <ul>
                 {comments && comments.length? 
                 comments.map(comment => <li><div>{comment.username}</div><div><p>{comment.content}</p></div></li>)
