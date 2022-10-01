33d94af4315dd7c0dafc075d69cea34ccf7fdfa9
Dylan King <dwkblue51@yahoo.com>
2022-09-30 21:22:07 -0500

    add SignUp to /sign-up route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { SignUp } from "./routes/SignUp";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
-  }
-])
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
